4fd7ed2bb6dfb3ec9a0908095ac45122d613b3f2
ForteScarlet <ForteScarlet@163.com>
2022-09-18 02:08:16 +0800

    refactor: fix some

--- plugins/suspend-transform-plugin-gradle/src/main/kotlin/love/forte/plugin/suspendtrans/gradle/SuspendTransformGradlePlugin.kt
+++ plugins/suspend-transform-plugin-gradle/src/main/kotlin/love/forte/plugin/suspendtrans/gradle/SuspendTransformGradlePlugin.kt
@@ -56,45 +56,46 @@ open class SuspendTransformGradlePlugin : KotlinCompilerPluginSupportPlugin {
 
 
 private fun SuspendTransformGradleExtension.toSubpluginOptions(): List<SubpluginOption> {
-    val jvm = this.jvm
-    val js = this.js
-    return listOf(
-        SubpluginOption("enabled", enabled.toString()),
-        SubpluginOption("includeRuntime", includeRuntime.toString()),
-        SubpluginOption("runtimeConfigurationName", runtimeConfigurationName),
-        // jvm
-        SubpluginOption("jvm.jvmBlockingFunctionName", jvm.jvmBlockingFunctionName),
-        SubpluginOption("jvm.jvmBlockingMarkAnnotation.annotationName", jvm.jvmBlockingMarkAnnotation.annotationName),
-        SubpluginOption("jvm.jvmBlockingMarkAnnotation.baseNameProperty", jvm.jvmBlockingMarkAnnotation.baseNameProperty),
-        SubpluginOption("jvm.jvmBlockingMarkAnnotation.suffixProperty", jvm.jvmBlockingMarkAnnotation.suffixProperty),
-        SubpluginOption("jvm.jvmBlockingMarkAnnotation.asPropertyProperty", jvm.jvmBlockingMarkAnnotation.asPropertyProperty),
-
-        SubpluginOption("jvm.jvmAsyncFunctionName", jvm.jvmAsyncFunctionName),
-        SubpluginOption("jvm.jvmAsyncMarkAnnotation.annotationName", jvm.jvmAsyncMarkAnnotation.annotationName),
-        SubpluginOption("jvm.jvmAsyncMarkAnnotation.baseNameProperty", jvm.jvmAsyncMarkAnnotation.baseNameProperty),
-        SubpluginOption("jvm.jvmAsyncMarkAnnotation.suffixProperty", jvm.jvmAsyncMarkAnnotation.suffixProperty),
-        SubpluginOption("jvm.jvmAsyncMarkAnnotation.asPropertyProperty", jvm.jvmAsyncMarkAnnotation.asPropertyProperty),
-
-//        SubpluginOption("jvm.originFunctionIncludeAnnotations", jvm.originFunctionIncludeAnnotations.toString()),
-
-//        SubpluginOption("jvm.syntheticBlockingFunctionIncludeAnnotations", jvm.syntheticBlockingFunctionIncludeAnnotations.toString()),
-        SubpluginOption("jvm.copyAnnotationsToSyntheticBlockingFunction", jvm.copyAnnotationsToSyntheticBlockingFunction.toString()),
-//        SubpluginOption("jvm.copyAnnotationsToSyntheticBlockingFunctionExcludes", jvm.copyAnnotationsToSyntheticBlockingFunctionExcludes.toString()),
-//        SubpluginOption("jvm.syntheticAsyncFunctionIncludeAnnotations", jvm.syntheticAsyncFunctionIncludeAnnotations.toString()),
-        SubpluginOption("jvm.copyAnnotationsToSyntheticAsyncFunction", jvm.copyAnnotationsToSyntheticAsyncFunction.toString()),
-//        SubpluginOption("jvm.copyAnnotationsToSyntheticAsyncFunctionExcludes", jvm.copyAnnotationsToSyntheticAsyncFunctionExcludes.toString()),
-
-        // js
-        SubpluginOption("js.jsPromiseFunctionName", js.jsPromiseFunctionName),
-        SubpluginOption("js.jsPromiseMarkAnnotation.annotationName", js.jsPromiseMarkAnnotation.annotationName),
-        SubpluginOption("js.jsPromiseMarkAnnotation.baseNameProperty", js.jsPromiseMarkAnnotation.baseNameProperty),
-        SubpluginOption("js.jsPromiseMarkAnnotation.suffixProperty", js.jsPromiseMarkAnnotation.suffixProperty),
-        SubpluginOption("js.jsPromiseMarkAnnotation.asPropertyProperty", js.jsPromiseMarkAnnotation.asPropertyProperty),
-
-//        SubpluginOption("js.originFunctionIncludeAnnotations", enabled.toString()),
-//        SubpluginOption("js.syntheticAsyncFunctionIncludeAnnotations", enabled.toString()),
-        SubpluginOption("js.copyAnnotationsToSyntheticAsyncFunction", js.copyAnnotationsToSyntheticAsyncFunction.toString()),
-//        SubpluginOption("js.copyAnnotationsToSyntheticAsyncFunctionExcludes", enabled.toString()),
-    )
+    return listOf()
+//    val jvm = this.jvm
+//    val js = this.js
+//    return listOf(
+//        SubpluginOption("enabled", enabled.toString()),
+//        SubpluginOption("includeTransformRuntime", includeTransformRuntime.toString()),
+//        SubpluginOption("runtimeConfigurationName", runtimeConfigurationName),
+//        // jvm
+//        SubpluginOption("jvm.jvmBlockingFunctionName", jvm.jvmBlockingFunctionName),
+//        SubpluginOption("jvm.jvmBlockingMarkAnnotation.annotationName", jvm.jvmBlockingMarkAnnotation.annotationName),
+//        SubpluginOption("jvm.jvmBlockingMarkAnnotation.baseNameProperty", jvm.jvmBlockingMarkAnnotation.baseNameProperty),
+//        SubpluginOption("jvm.jvmBlockingMarkAnnotation.suffixProperty", jvm.jvmBlockingMarkAnnotation.suffixProperty),
+//        SubpluginOption("jvm.jvmBlockingMarkAnnotation.asPropertyProperty", jvm.jvmBlockingMarkAnnotation.asPropertyProperty),
+//
+//        SubpluginOption("jvm.jvmAsyncFunctionName", jvm.jvmAsyncFunctionName),
+//        SubpluginOption("jvm.jvmAsyncMarkAnnotation.annotationName", jvm.jvmAsyncMarkAnnotation.annotationName),
+//        SubpluginOption("jvm.jvmAsyncMarkAnnotation.baseNameProperty", jvm.jvmAsyncMarkAnnotation.baseNameProperty),
+//        SubpluginOption("jvm.jvmAsyncMarkAnnotation.suffixProperty", jvm.jvmAsyncMarkAnnotation.suffixProperty),
+//        SubpluginOption("jvm.jvmAsyncMarkAnnotation.asPropertyProperty", jvm.jvmAsyncMarkAnnotation.asPropertyProperty),
+//
+////        SubpluginOption("jvm.originFunctionIncludeAnnotations", jvm.originFunctionIncludeAnnotations.toString()),
+//
+////        SubpluginOption("jvm.syntheticBlockingFunctionIncludeAnnotations", jvm.syntheticBlockingFunctionIncludeAnnotations.toString()),
+//        SubpluginOption("jvm.copyAnnotationsToSyntheticBlockingFunction", jvm.copyAnnotationsToSyntheticBlockingFunction.toString()),
+////        SubpluginOption("jvm.copyAnnotationsToSyntheticBlockingFunctionExcludes", jvm.copyAnnotationsToSyntheticBlockingFunctionExcludes.toString()),
+////        SubpluginOption("jvm.syntheticAsyncFunctionIncludeAnnotations", jvm.syntheticAsyncFunctionIncludeAnnotations.toString()),
+//        SubpluginOption("jvm.copyAnnotationsToSyntheticAsyncFunction", jvm.copyAnnotationsToSyntheticAsyncFunction.toString()),
+////        SubpluginOption("jvm.copyAnnotationsToSyntheticAsyncFunctionExcludes", jvm.copyAnnotationsToSyntheticAsyncFunctionExcludes.toString()),
+//
+//        // js
+//        SubpluginOption("js.jsPromiseFunctionName", js.jsPromiseFunctionName),
+//        SubpluginOption("js.jsPromiseMarkAnnotation.annotationName", js.jsPromiseMarkAnnotation.annotationName),
+//        SubpluginOption("js.jsPromiseMarkAnnotation.baseNameProperty", js.jsPromiseMarkAnnotation.baseNameProperty),
+//        SubpluginOption("js.jsPromiseMarkAnnotation.suffixProperty", js.jsPromiseMarkAnnotation.suffixProperty),
+//        SubpluginOption("js.jsPromiseMarkAnnotation.asPropertyProperty", js.jsPromiseMarkAnnotation.asPropertyProperty),
+//
+////        SubpluginOption("js.originFunctionIncludeAnnotations", enabled.toString()),
+////        SubpluginOption("js.syntheticAsyncFunctionIncludeAnnotations", enabled.toString()),
+//        SubpluginOption("js.copyAnnotationsToSyntheticAsyncFunction", js.copyAnnotationsToSyntheticAsyncFunction.toString()),
+////        SubpluginOption("js.copyAnnotationsToSyntheticAsyncFunctionExcludes", enabled.toString()),
+//    )
 
 }
