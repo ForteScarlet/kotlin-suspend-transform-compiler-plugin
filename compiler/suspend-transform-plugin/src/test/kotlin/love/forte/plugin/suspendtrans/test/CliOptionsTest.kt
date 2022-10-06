02bc3f37af46849e136fbba7c6d0495026049af7
ForteScarlet <ForteScarlet@163.com>
2022-10-06 20:01:09 +0800

    test: CliOptions test

--- compiler/suspend-transform-plugin/src/test/kotlin/love/forte/plugin/suspendtrans/test/CliOptionsTest.kt
+++ compiler/suspend-transform-plugin/src/test/kotlin/love/forte/plugin/suspendtrans/test/CliOptionsTest.kt
@@ -1,7 +1,9 @@
 package love.forte.plugin.suspendtrans.test
 
 import love.forte.plugin.suspendtrans.CliOptions
+import love.forte.plugin.suspendtrans.ICliOption
 import love.forte.plugin.suspendtrans.SuspendTransformConfiguration
+import kotlin.reflect.KMutableProperty1
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -12,6 +14,18 @@ import kotlin.test.assertEquals
  */
 class CliOptionsTest {
 
+    private fun <T> jvmConfigFormatTest(property: KMutableProperty1<SuspendTransformConfiguration.Jvm, T>, value: T, option: ICliOption) {
+        val originConfig = SuspendTransformConfiguration().apply {
+            property.set(jvm, value)
+        }
+
+        val valueString = option.resolveToValue(originConfig)
+        val newConfig = SuspendTransformConfiguration()
+        option.resolveFromValue(newConfig, valueString)
+        assertEquals(value, property.get(newConfig.jvm))
+
+    }
+
     @Test
     fun formatTest() {
         val annotations = listOf(
@@ -19,18 +33,10 @@ class CliOptionsTest {
             SuspendTransformConfiguration.IncludeAnnotation("World", true)
         )
 
-        val config = SuspendTransformConfiguration().apply {
-            jvm {
-                syntheticBlockingFunctionIncludeAnnotations = annotations
-            }
-        }
+        jvmConfigFormatTest(SuspendTransformConfiguration.Jvm::syntheticBlockingFunctionIncludeAnnotations, annotations, CliOptions.Jvm.SYNTHETIC_BLOCKING_FUNCTION_INCLUDE_ANNOTATIONS)
+        jvmConfigFormatTest(SuspendTransformConfiguration.Jvm::syntheticAsyncFunctionIncludeAnnotations, annotations, CliOptions.Jvm.SYNTHETIC_ASYNC_FUNCTION_INCLUDE_ANNOTATIONS)
 
-        CliOptions.Jvm.SYNTHETIC_BLOCKING_FUNCTION_INCLUDE_ANNOTATIONS.also { opt ->
-            val value = opt.resolveToValue(config)
-            val newConfig = SuspendTransformConfiguration()
-            opt.resolveFromValue(newConfig, value)
-            assertEquals(annotations, newConfig.jvm.syntheticBlockingFunctionIncludeAnnotations)
-        }
+        jvmConfigFormatTest(SuspendTransformConfiguration.Jvm::originFunctionIncludeAnnotations, annotations, CliOptions.Jvm.ORIGIN_FUNCTION_INCLUDE_ANNOTATIONS)
 
 
     }
