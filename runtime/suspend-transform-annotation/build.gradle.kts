63f77d688d8b1a72831e537dd1b9437ee76b9e4b
ForteScarlet <ForteScarlet@163.com>
2023-03-29 21:21:19 +0800

    release: v0.2.3

--- runtime/suspend-transform-annotation/build.gradle.kts
+++ runtime/suspend-transform-annotation/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 kotlin {
     explicitApi()
-    
+
     jvm {
         compilations.all {
             kotlinOptions {
@@ -20,10 +20,29 @@ kotlin {
     js(IR) {
         nodejs()
     }
+    val mainPresets = mutableSetOf<org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet>()
+    val testPresets = mutableSetOf<org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet>()
+
+    targets {
+        presets
+            .filterIsInstance<org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinNativeTargetPreset<*>>()
+            .forEach { presets ->
+                val target = fromPreset(presets, presets.name)
+                mainPresets.add(target.compilations["main"].kotlinSourceSets.first())
+                testPresets.add(target.compilations["test"].kotlinSourceSets.first())
+            }
+    }
 
     configAllNativeTargets()
 
     sourceSets {
         val commonMain by getting
+        val commonTest by getting
+
+        val nativeMain by creating { dependsOn(commonMain) }
+        val nativeTest by creating { dependsOn(commonTest) }
+
+        configure(mainPresets) { dependsOn(nativeMain) }
+        configure(testPresets) { dependsOn(nativeTest) }
     }
 }
