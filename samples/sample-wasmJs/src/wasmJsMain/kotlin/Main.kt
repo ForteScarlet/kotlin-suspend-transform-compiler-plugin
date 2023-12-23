2f537833f4ce1d2ba9eb5456991ac645ea3faa3a
ForteScarlet <ForteScarlet@163.com>
2023-12-23 19:41:35 +0800

    support `wasmJs` target

--- samples/sample-wasmJs/src/wasmJsMain/kotlin/Main.kt
+++ samples/sample-wasmJs/src/wasmJsMain/kotlin/Main.kt
@@ -3,6 +3,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.CanvasBasedWindow
@@ -14,7 +15,7 @@ fun main() {
     Promise // keep import
     CanvasBasedWindow(canvasElementId = "ComposeTarget") {
         var asyncValue by remember { mutableStateOf<JsAny?>(null) }
-        Column(Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center) {
+        Column(Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
             Text(ForteScarlet().toString())
             Text(ForteScarlet()::stringToInt.toString())
             Text(asyncValue.toString())
